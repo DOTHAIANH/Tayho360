9af204f29ab4c467a759952ffd6963f7d36826ef
DOTHAIANH <97963238+DOTHAIANH@users.noreply.github.com>
2023-03-20 14:34:37 +0700

    aaaaa

--- frontend/js/slickslide.js
+++ frontend/js/slickslide.js
@@ -1,12 +1,31 @@
-$jq = jQuery;
+// console.log('a');
 
-$jq(document).ready(function () {
+// const $ = jQuery ;
+
+$(document).ready(function () {
 
     $('.in-box-event-home').slick({
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        prevArrow:`<button type='button' class='slick-prev pull-left'><i class="fa-solid fa-angle-left"></i></button>`,
+        nextArrow:`<button type='button' class='slick-next pull-right'><i class="fa-solid fa-angle-right"></i></button>`
     });
 });
+
+$(document).ready(function () {
+
+    $('.slide-time-event').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        speed: 500,
+        prevArrow:`<button type='button' class='slick-prev pull-left'><i class="fa-solid fa-angle-left"></i></button>`,
+        nextArrow:`<button type='button' class='slick-next pull-right'><i class="fa-solid fa-angle-right"></i></button>`
+    });
+
+});
+
+
