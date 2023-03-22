bef2dc7ba0bdc377416e74c5d2a8cc42888b9e8c
hieugiataytay <hieuugia20@gmail.com>
2023-03-23 01:15:24 +0700

    aaaaaaaa

--- frontend/js/slick.js
+++ frontend/js/slick.js
@@ -43,7 +43,33 @@ $( document ).ready(function() {
     slidesToShow: 3,
     slidesToScroll: 3,
     prevArrow:`<button type='button' class='slick-prev pull-left'><i class="fa-solid fa-angle-left"></i></button>`,
-    nextArrow:`<button type='button' class='slick-next pull-right'><i class="fa-solid fa-angle-right"></i></button>`
+    nextArrow:`<button type='button' class='slick-next pull-right'><i class="fa-solid fa-angle-right"></i></button>`,
+    responsive: [
+      {
+        breakpoint: 1060,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+
+      {
+        breakpoint: 1060,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  
   });
   
 
