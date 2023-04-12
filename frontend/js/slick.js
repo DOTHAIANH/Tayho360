28c95283b67b18e15fe1382bdfe5ce127d5dfb8c
hieugiataytay <hieuugia20@gmail.com>
2023-04-13 03:32:26 +0700

    aaaaa

--- frontend/js/slick.js
+++ frontend/js/slick.js
@@ -95,6 +95,40 @@ $( document ).ready(function() {
     asNavFor:'.artifacts-slide-top',
   });
 
+
+  $('.place-artifacts-slide').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:`<button type='button' class='slick-prev pull-left'><i class="fa-solid fa-angle-left"></i></button>`,
+    nextArrow:`<button type='button' class='slick-next pull-right'><i class="fa-solid fa-angle-right"></i></button>`,
+    responsive: [
+      {
+        breakpoint: 1060,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+
+      {
+        breakpoint: 1060,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
 });
 
 
